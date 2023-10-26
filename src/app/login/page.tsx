8d8245da7936069debb96624f80af9b9fe0b6fdb
yahao <megabaitzz@gmail.com>
2023-10-26 14:09:41 +0200

    clean

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,28 +17,10 @@ export default function LoginPage() {
             <h1 className="text-2xl font-semibold tracking-tight">
               Iniciar sesión
             </h1>
-            {/*<p className="text-sm text-muted-foreground">*/}
-            {/*  Enter your email below to create your account*/}
-            {/*</p>*/}
+
           </div>
           <UserAuthForm/>
-          {/*<p className="px-8 text-center text-sm text-muted-foreground">*/}
-          {/*  By clicking continue, you agree to our{" "}*/}
-          {/*  <Link*/}
-          {/*    href="/terms"*/}
-          {/*    className="underline underline-offset-4 hover:text-primary"*/}
-          {/*  >*/}
-          {/*    Terms of Service*/}
-          {/*  </Link>{" "}*/}
-          {/*  and{" "}*/}
-          {/*  <Link*/}
-          {/*    href="/privacy"*/}
-          {/*    className="underline underline-offset-4 hover:text-primary"*/}
-          {/*  >*/}
-          {/*    Privacy Policy*/}
-          {/*  </Link>*/}
-          {/*  .*/}
-          {/*</p>*/}
+
         </div>
       </div>
     )
